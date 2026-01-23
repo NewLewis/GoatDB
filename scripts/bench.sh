9e94d637bdbffc9ed750cba28dc56f55a193a7e2
NewLewis <rui1218911655@gmail.com>
2026-01-23 18:32:00 +0800

    Add benchmark tool and disable WAL sync by default

---
+++ scripts/bench.sh
@@ -0,0 +1,2 @@
+cargo flamegraph --release --features rocksdb --bin goatkv_bench --   --directory ./bench_data   --threads 1   --engine both   populate --key-nums 100000 --batch-size 1000 --value-size 1024 --seq
+perf script -i perf.data | inferno-collapse-perf | inferno-flamegraph > flamegraph.svg
